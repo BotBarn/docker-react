8432b00455d4f2e01c56fb58c810f123772d1b13
Richard Bondarev <richard.bondarev@sl-028077.labda.sva.de>
2025-04-19 20:54:10 +0000

    App Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Das ist Mega cool!
+          Demo App
         </p>
         <a
           className="App-link"
